342059ac07eb73bbc26d7bfee851919c94e84e8c
Tammy Lam <lamtammy1515@gmail.com>
2020-04-06 00:52:13 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,6 +14,8 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    self.all.artist.name
+    self.artist.collect do |m|
+      m.names 
+    end 
   end
 end
